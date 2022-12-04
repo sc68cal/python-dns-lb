0347d327cb79bfd98599ec1f4399df16ac75de12
Sean M. Collins <sean@coreitpro.com>
2022-12-03 23:19:29 -0500

    Implement round robin via database table
    This uses a database table to store state and what host
    was used for a DNS reply previously

--- server.py
+++ server.py
@@ -15,10 +15,12 @@ except ImportError:
     pass
 
 server_query = """
-SELECT host FROM dns_zone_member
+SELECT dns_zone_member.id, host FROM dns_zone_member
 JOIN dns_zone ON dns_zone_member.dns_zone = dns_zone.id
 WHERE dns_zone.name=?
 """
+
+db_host = '127.0.0.1'
 
 
 class DnsServerProtocol:
@@ -36,12 +38,21 @@ class DnsServerProtocol:
         print('Received %r from %s' % (dq, addr))
 
         domain = dq.question[0].name.to_text()
+
+        domain_id = await self.get_domain_id(domain)
+
         servers = await self.get_servers_for_domain(domain)
+
+        exclude = await self.get_last_server_round_robin(domain_id)
+
+        to_check = list(filter(lambda x: x['id'] != exclude, servers))
+
         checks = []
-        for server in servers:
-            checks.append(self.check_server(server))
+        for server in to_check:
+            checks.append(self.check_server(server['host']))
 
         results = await asyncio.gather(*checks)
+
         output = message.make_response(dq)
         rdclass = dns.rdataclass.from_text('IN')
         rdtype = dns.rdatatype.A
@@ -49,12 +60,14 @@ class DnsServerProtocol:
 
         records = []
 
-        # TODO - implement round-robin instead of returning all
         for server, alive in results:
             if alive:
                 records.append(A_RECORD(rdclass, rdtype, server))
+                server_id = list(filter(lambda x: x['host'] == server, servers))[0]['id']
+                await self.update_server_round_robin(server_id, domain_id)
+                break
 
-        rrset = dns.rrset.from_rdata(dnsname, 9600,*records)
+        rrset = dns.rrset.from_rdata(dnsname, 9600, *records)
         output.answer.append(rrset)
 
         print('Send %r to %s' % (output, addr))
@@ -68,10 +81,37 @@ class DnsServerProtocol:
     async def get_servers_for_domain(self, domain):
         query = [[server_query, domain]]
         async with aiohttp.ClientSession() as session:
-            async with session.post("http://127.0.0.1:4001/db/query?associative",
+            async with session.post(f"http://{db_host}:4001/db/query?associative",
                                     json=query) as resp:
                 data = await resp.json()
-                return [x['host'] for x in data['results'][0]['rows']]
+                return data['results'][0]['rows']
+
+    async def get_domain_id(self, domain):
+        query = [["SELECT id FROM dns_zone WHERE name=?", domain]]
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"http://{db_host}:4001/db/query",
+                                    json=query) as resp:
+                data = await resp.json()
+                return data['results'][0]['values'][0][0]
+
+    async def get_last_server_round_robin(self, domain_id):
+        query = [["SELECT dns_zone_member FROM \
+                  rr_state JOIN dns_zone ON \
+                  rr_state.dns_zone = dns_zone.id WHERE dns_zone.id=?",
+                  domain_id]]
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"http://{db_host}:4001/db/query",
+                                    json=query) as resp:
+                data = await resp.json()
+                return data['results'][0]["values"][0][0]
+
+    async def update_server_round_robin(self, member_id, domain_id):
+        query = [['UPDATE rr_state SET dns_zone_member=? WHERE dns_zone=?',
+                 member_id, domain_id]]
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"http://{db_host}:4001/db/execute",
+                                    json=query) as resp:
+                await resp.json()
 
 
 def main():
